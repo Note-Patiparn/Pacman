45b7bcc89b4dc1a38912641ab0358f11c4eac273
Note Patiparn <notexpen@gmail.com>
2021-04-27 04:06:06 +0700

    pacman html & css structure

---
+++ script.js
@@ -0,0 +1,90 @@
+const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector('#scoreboard')
+let blocks = []
+const width = 28;
+
+
+  // 0 - pac-dots
+  // 1 - wall
+  // 2 - ghost-lair
+  // 3 - power-pellet
+  // 4 - empty
+
+  // to improve > the output > setting new class for wall'edge 
+  // to make the wall and game look more spae 
+
+  const layout = [
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 
+]
+
+const createGrid = () => {
+    for (let i = 0; i < layout.length; i++) {
+        // create block
+        const block = document.createElement('div')
+        // adding styling to each block
+        // block.classList.add('block')
+        grid.appendChild(block)
+        blocks.push(block)
+        // console.log(blocks)
+        if(layout[i] === 0) {
+            blocks[i].classList.add('pac-dot')
+        }else if(layout[i] === 1) {
+            blocks[i].classList.add('wall')
+        }else if(layout[i] === 3) {
+            blocks[i].classList.add('power-pellet')
+        }else if(layout[i] === 4) {
+        }
+    }
+}
+
+createGrid()
+console.log(blocks.length)
+
+// Pacman positioning 
+let pacmanCurrentIndex = 489;
+
+blocks[pacmanCurrentIndex].classList.add('pacman')
+
+// Moving pacman  with arrow key || fps controler mode
+// By using this ref. to refer to each arrow key https://keycode.info/
+function control(e) {
+    if(e.keyCode === 37 || e.keyCode === 65) {
+        console.log('left arrow')
+    }else if(e.keyCode === 38 || e.keyCode === 87) {
+        console.log('up arrow')
+    }else if(e.keyCode === 39 || e.keyCode === 68) {
+        console.log('right arrow')
+    }else if(e.keyCode === 40 || e.keyCode === 83) {
+        console.log('down arrow')
+    }
+}
+
+
+document.addEventListener('keydown', control)
